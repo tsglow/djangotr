e0725cebea41337978554844a2e55fb09408c075
dinner_rolls <haruki84@msn.com>
2026-04-29 13:56:04 +0900

    feat: add include

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index),
+    path("", views.index, name="index"),
     path("<int:month>", views.num_redirect),
     path("<str:month>", views.mon_challenge, name="month-challenge")
 ]
